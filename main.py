7adade56746989a95dd61b551e524caf37cca83d
Krishnapriyabala <krishnapriyabal.js.2024.ece@rajalakshmi.edu.in>
2026-01-02 14:38:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu1 = Menu()
+
+
+line = True
+while line:
+    options = menu1.get_items()
+    choice = input(f"What would you like? ({options}):")
+    if choice == "off":
+        line = False
+
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu1.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
+
+        
+
+
+
+
+
